4a30528daf511f667134e0c8912e722c658fd351
KilpikonnanKerma <rajalanico08@gmail.com>
2025-09-12 13:06:43 +0300

    started making a window library

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,58 @@
-
-#include "math.hpp"
+#include "nswindow.hpp"
 #include <iostream>
+#include <cmath>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+#include <gl/GL.h>
+
+using namespace NSWindow;
 
 int main() {
-    using namespace NMATH;
+	Window window(WindowDesc{ "OpenGL Triangle", 800, 600, true, false });
 
-    Vec2d v(3, 4);
-    std::cout << "Length = " << v.length() << "\n";
+	// Create OpenGL context if not already created in your header
+	window.makeGLCurrent();
+
+	float angle = 0.0f;
+
+	while (!window.shouldClose()) {
+		window.pollEvents();
+
+		NSWindow::Event e;
+		while (window.pollEvent(e)) {
+			if (e.type == EventType::Resize) {
+				glViewport(0, 0, e.size.w, e.size.h);
+			}
+		}
+
+		// Rotate with left/right arrow keys
+		if (window.keyDown(VK_LEFT))  angle -= 1.0f;
+		if (window.keyDown(VK_RIGHT)) angle += 1.0f;
+
+		// OpenGL rendering
+		glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-1, 1, -1, 1, -1, 1);
+
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
+		glBegin(GL_TRIANGLES);
+		glColor3f(1, 0, 0); glVertex2f(0.0f, 0.5f);
+		glColor3f(0, 1, 0); glVertex2f(-0.5f, -0.5f);
+		glColor3f(0, 0, 1); glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		// Swap buffers
+		SwapBuffers((HDC)window.nativeHandle());
+	}
+
+	return 0;
 }
---
+++ window/nswindow.hpp
@@ -0,0 +1,319 @@
+/*
+
+	Copyright (C) 2025 Nico Rajala. All rights reserved.
+
+	NSWindow.hpp
+	A simple cross-platform (win32 currently so not yet ;) ) windowing library using native APIs.
+
+	Use at your own risk. No warranties are provided.
+
+*/
+
+#ifndef NSWINDOW_HPP
+#define NSWINDOW_HPP
+
+#include <string>
+#include <stdexcept>
+#include <queue>
+
+#include <GL/gl.h>
+
+#if defined(_WIN32)
+#ifndef GET_X_LPARAM
+#define GET_X_LPARAM(lp) ((int)(short)LOWORD(lp))
+#endif
+#ifndef GET_Y_LPARAM
+#define GET_Y_LPARAM(lp) ((int)(short)HIWORD(lp))
+#endif
+
+#include <windows.h>
+#endif
+
+namespace NSWindow {
+
+	enum class EventType {
+		None, Close, Resize, Move, FocusGained, FocusLost, Minimized, Maximized, Restored
+	};
+
+	struct Event {
+		EventType type;
+		union {
+			struct { int w, h; } size;
+			struct { int x, y; } pos;
+		};
+	};
+
+	struct WindowDesc {
+		std::string title = "NSWindow";
+		int width = 800;
+		int height = 600;
+
+		bool resizable = true;
+		bool fullscreen = false;
+		bool borderless = false;
+	};
+
+	class Window {
+	public:
+		Window(const WindowDesc& desc) {
+			impl = new Impl(desc);
+		}
+
+		~Window() {
+			delete impl;
+		}
+		
+		void pollEvents() { impl->pollEvents(); }
+
+		bool shouldClose() const { return impl->shouldClose; }
+
+		void* nativeHandle() const { return impl->nativeHandle(); }
+
+		void setTitle(const char* title) { impl->setTitle(title); }
+		const char* getTitle() const { return impl->title.c_str(); }
+
+		void setPosition(int x, int y) { impl->setPosition(x, y); }
+		void getPosition(int& x, int& y) const { impl->getPosition(x, y); }
+
+		void setSize(int w, int h) { impl->setSize(w, h); }
+		void getSize(int& w, int& h) const { impl->getSize(w, h); }
+
+		bool keyDown(int key) const { return impl->keyDown(key); }
+
+		bool mouseButtonDown(int button) const { return impl->mouseButtonDown(button); }
+		void getMousePosition(int& x, int& y) const { impl->getMousePosition(x, y); }
+
+		void showCursor(bool show) { impl->showCursor(show); }
+		void setCursorPos(int x, int y) { impl->setCursorPos(x, y); }
+		bool pollEvent(Event& e) { return impl->pollEvent(e); }
+
+		void makeGLCurrent() { impl->makeGLCurrent(); }
+		void swapBuffers() { impl->swapBuffers(); }
+
+	private:
+		struct Impl {
+			HWND hwnd = nullptr;
+			std::string title;
+			bool shouldClose = false;
+			bool keys[256] = {};
+			bool mouseButtons[5] = {};
+			int mouseX = 0, mouseY = 0;
+			int mouseWheel = 0, mouseHWheel = 0;
+			std::string textInputBuffer;
+			std::queue<Event> eventQueue;
+
+			HDC hdc = nullptr;
+			HGLRC hglrc = nullptr;
+
+			static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+				Impl* self = reinterpret_cast<Impl*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+
+				switch (uMsg) {
+				case WM_CLOSE:
+					if (self) {
+						self->shouldClose = true;
+						self->eventQueue.push({EventType::Close});
+					}
+					break;
+				case WM_KEYDOWN:
+					if (self && wParam < 256) self->keys[wParam] = true;
+					break;
+				case WM_KEYUP:
+					if (self && wParam < 256) self->keys[wParam] = false;
+					break;
+				case WM_LBUTTONDOWN:
+					if (self) self->mouseButtons[0] = true;
+					break;
+				case WM_LBUTTONUP:
+					if (self) self->mouseButtons[0] = false;
+					break;
+				case WM_RBUTTONDOWN:
+					if (self) self->mouseButtons[1] = true;
+					break;
+				case WM_RBUTTONUP:
+					if (self) self->mouseButtons[1] = false;
+					break;
+				case WM_MBUTTONDOWN:
+					if (self) self->mouseButtons[2] = true;
+					break;
+				case WM_MBUTTONUP:
+					if (self) self->mouseButtons[2] = false;
+					break;
+				case WM_MOUSEMOVE:
+					if (self) {
+						self->mouseX = GET_X_LPARAM(lParam);
+						self->mouseY = GET_Y_LPARAM(lParam);
+					}
+					break;
+				case WM_MOUSEWHEEL:
+					if (self) self->mouseWheel += GET_WHEEL_DELTA_WPARAM(wParam) / WHEEL_DELTA;
+					break;
+				case WM_MOUSEHWHEEL:
+					if (self) self->mouseHWheel += GET_WHEEL_DELTA_WPARAM(wParam) / WHEEL_DELTA;
+					break;
+				case WM_CHAR:
+				    if (self) self->textInputBuffer.push_back((char)wParam);
+				    break;
+				case WM_SIZE:
+				    if (self) {
+				        int w = LOWORD(lParam), h = HIWORD(lParam);
+				        self->eventQueue.push({EventType::Resize, {{w, h}}});
+				        if (wParam == SIZE_MINIMIZED)
+				            self->eventQueue.push({EventType::Minimized});
+				        else if (wParam == SIZE_MAXIMIZED)
+				            self->eventQueue.push({EventType::Maximized});
+				        else if (wParam == SIZE_RESTORED)
+				            self->eventQueue.push({EventType::Restored});
+				    }
+				    break;
+				case WM_MOVE:
+				    if (self) {
+				        int x = (int)(short)LOWORD(lParam), y = (int)(short)HIWORD(lParam);
+				        self->eventQueue.push({EventType::Move, {{x, y}}});
+				    }
+				    break;
+				case WM_SETFOCUS:
+				    if (self) self->eventQueue.push({EventType::FocusGained});
+				    break;
+				case WM_KILLFOCUS:
+				    if (self) self->eventQueue.push({EventType::FocusLost});
+				    break;
+				default:
+					break;
+				}
+				return DefWindowProc(hwnd, uMsg, wParam, lParam);
+			}
+
+			Impl(const WindowDesc& desc) : title(desc.title) {
+				WNDCLASS wc = {};
+				wc.lpfnWndProc = WindowProc;
+				wc.hInstance = GetModuleHandle(nullptr);
+				wc.lpszClassName = "NSWindowClass";
+				RegisterClass(&wc);
+
+				RECT rect = { 0, 0, desc.width, desc.height };
+				AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+				DWORD style = WS_OVERLAPPEDWINDOW;
+				if (!desc.resizable) style &= ~WS_THICKFRAME;
+				if (desc.borderless) style &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZE | WS_MAXIMIZE | WS_SYSMENU);
+
+				hwnd = CreateWindowExW(
+					0, L"NSWindowClass", std::wstring(title.begin(), title.end()).c_str(),
+					style | WS_VISIBLE,
+					CW_USEDEFAULT, CW_USEDEFAULT,
+					rect.right - rect.left, rect.bottom - rect.top,
+					nullptr, nullptr, GetModuleHandle(nullptr), nullptr);
+
+				if (!hwnd) {
+					throw std::runtime_error("[NSWindow] Failed to create window");
+				}
+
+				SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+			}
+
+			~Impl() {
+				if (hwnd) {
+					DestroyWindow(hwnd);
+				}
+			}
+
+			void pollEvents() {
+				MSG msg;
+				while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+					TranslateMessage(&msg);
+					DispatchMessage(&msg);
+				}
+			}
+
+			bool pollEvent(Event& e) {
+				if (eventQueue.empty()) return false;
+				e = eventQueue.front();
+				eventQueue.pop();
+				return true;
+			}
+
+			void setTitle(const char* newTitle) {
+				title = newTitle;
+				SetWindowText(hwnd, title.c_str());
+			}
+
+			void setPosition(int x, int y) { SetWindowPos(hwnd, nullptr, x, y, 0, 0, SWP_NOSIZE | SWP_NOZORDER); }
+			void getPosition(int& x, int& y) const {
+				RECT r; GetWindowRect(hwnd, &r); x = r.left; y = r.top;
+			}
+
+			void setSize(int w, int h) { SetWindowPos(hwnd, nullptr, 0, 0, w, h, SWP_NOMOVE | SWP_NOZORDER); }
+			void getSize(int& w, int& h) const {
+				RECT r; GetClientRect(hwnd, &r); w = r.right - r.left; h = r.bottom - r.top;
+			}
+
+			bool keyDown(int key) const {
+				if (key < 0 || key >= 256) return false;
+				return keys[key];
+			}
+
+			bool mouseButtonDown(int button) const {
+				if (button < 0 || button >= 5) return false;
+				return mouseButtons[button];
+			}
+
+			void showCursor(bool show) { ShowCursor(show); }
+			void setCursorPos(int x, int y) { POINT pt = { x, y }; ClientToScreen(hwnd, &pt); SetCursorPos(pt.x, pt.y); }
+
+			void getMousePosition(int& x, int& y) const {
+				x = mouseX;
+				y = mouseY;
+			}
+
+			void* nativeHandle() const {
+				return hwnd;
+			}
+
+			// OpenGL context management
+
+			void createGLContext() {
+				hdc = GetDC(hwnd);
+
+				PIXELFORMATDESCRIPTOR pfd = {};
+				pfd.nSize = sizeof(pfd);
+				pfd.nVersion = 1;
+				pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+				pfd.iPixelType = PFD_TYPE_RGBA;
+				pfd.cColorBits = 32;
+				pfd.cDepthBits = 24;
+				pfd.cStencilBits = 8;
+				pfd.iLayerType = PFD_MAIN_PLANE;
+
+				int pf = ChoosePixelFormat(hdc, &pfd);
+				SetPixelFormat(hdc, pf, &pfd);
+
+				hglrc = wglCreateContext(hdc);
+				wglMakeCurrent(hdc, hglrc);
+			}
+
+			void destroyGLContext() {
+				if (hglrc) {
+					wglMakeCurrent(nullptr, nullptr);
+					wglDeleteContext(hglrc);
+					hglrc = nullptr;
+				}
+				if (hdc) {
+					ReleaseDC(hwnd, hdc);
+					hdc = nullptr;
+				}
+			}
+
+			void makeGLCurrent() { wglMakeCurrent(hdc, hglrc); }
+
+			void swapBuffers() {
+				SwapBuffers(hdc);
+			}
+		};
+
+
+		Impl* impl;
+	};
+}
+
+#endif
